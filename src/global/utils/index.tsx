ed5ffa7d0437d341409bc5b0bfddba870d5102d8
Marcelo <programadormarcelo1@gmail.com>
2023-02-25 18:39:34 -0300

    aula 8

---
+++ src/global/utils/index.tsx
@@ -0,0 +1,28 @@
+export const Teste = (valorDigitado: string) => {
+    if (valorDigitado === 'marcelo') {
+        console.log('trabalha na FPR');
+    }
+    else if (valorDigitado === 'karina') {
+        console.log('É brava');
+    }
+    else if (valorDigitado === 'duda') {
+        console.log('É preguisoza');
+    }
+    else if (valorDigitado === 'davi') {
+        console.log('É Chorão');
+    }
+    else if (valorDigitado === 'theo') {
+        console.log('É Bravo');
+    }
+    else {
+        console.log('Não atendeu os Requisitos');
+    }
+}
+
+export const Click = (nome: string) => {
+    switch (nome) {
+        case 'Bom': 'é'
+        case 'Bom': 'é'
+        case 'Bom': 'é'
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.scss'
+import { Click, Teste } from '@/global/utils'
 
 export default function Home() {
 
@@ -11,7 +12,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     
+      <form className={styles.box}>
+      <label>Digite Aqui:</label>
+      <input type="text" onChange={(e: any) => {Teste(e.target.value)}} />
+      <button onClick={(e) => {Click('marcelo')}}>Enviar</button>
+     </form>
     </div>
   )
 }
