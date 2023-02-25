62ed76be58bdce2bb5194a1a8f822ac672718afa
Marcelo <programadormarcelo1@gmail.com>
2023-02-25 16:58:45 -0300

    aula 6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,101 +2,31 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.scss'
 
 export default function Home() {
+ // onClick no click
+  // onChange na mudança
 
-  const familia = [
-    {
-      nome: 'Marcelo',
-      idade: '31 anos',
-      data_de_nascimento: '14/01/1922',
-      nome_mae: 'Maria Elena',
-      trabalha: true
-    },
-    {
-      nome: 'Karina',
-      idade: '26 anos',
-      data_de_nascimento: '17/06/1996',
-      nome_mae: 'Valdirene',
-      trabalha: true
-    },
-    {
-      nome: 'Eduarda',
-      idade: '11 anos',
-      data_de_nascimento: '30/06/2011',
-      nome_mae: 'Karina',
-      trabalha: false
-    },
-    {
-      nome: 'Davi Marcelo',
-      idade: '8 anos',
-      data_de_nascimento: '15/06/2014',
-      nome_mae: 'Karina',
-      trabalha: false
-    }
-  ]
+  // fazer um input com onChange que o a pessoa digitar ele vai somar com uma varivael
+  // com o valor 4 e vai retornar o alert com o resultado
 
-  const favorito = [
-    {
-      nome: 'Marcelo',
-      fruta: 'ABACAXI',
-      data_de_nascimento: '14/01/1922',
-    },
-    {
-      nome: 'Karina',
-      fruta: 'ABACAXI',
-      data_de_nascimento: '17/06/1996'      
-    },
-    {
-      nome: 'Eduarda',
-      fruta: 'UVA',
-      data_de_nascimento: '30/06/2011'    
-    },
- 
-  ]
 
-  const filtrando = familia.filter(item => {
-    return item.trabalha === false 
-  })
+//fazer um click numa div com texto qualquer que retorne um alert o texto "Você conseguiu" 
+// de uma variavel
   
-  const filtrandoFruta = favorito.filter(item => {
-    return item.fruta === 'ABACAXI' 
-  })
-
-  console.log(filtrando, 'filtrando2');
-
-//Listagem com 4 objetos cada objeto contendo 3 itens diversos e item boolean
-//só 2 sejam true e 2 vao ser false e que exiba os itens que tem "false"
-
-//outra lista que tenha 3 objetos com 3 string e só vão dois itens que a string sejá abacaxi
-
   return (
     <div className={styles.container}>
       <Head>
-        <title>Aula-React-FPR | Filter</title>
+        <title>Aula-React-FPR | onChange e onClick</title>
         <meta name="description" content="Aula-React-FPR" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {filtrando.map((item, index) => {
-        return (
-          <div className={styles.box}
-            key={index}>
-            <p>{item.nome}</p>
-            <p>{item.data_de_nascimento}</p>
-            <p>{item.nome_mae}</p>
-          </div>
-        )
-      })}
 
-       {filtrandoFruta.map((item, index) => {
-        return (
-          <div className={styles.box}
-            key={index}>
-            <p>{item.nome}</p>
-            <p>{item.fruta}</p>
-            <p>{item.data_de_nascimento}</p>
-          </div>
-        )
-      })}
+     <form className={styles.box}>
+      <label>Nome:</label>
+      <input type="text" onChange={(e) => console.log(e.target.value)} />
+      <button onClick={() => {alert('você enviou a requisição')}}>Enviar</button>
+     </form>
+     <p onClick={() => {alert('você enviou a requisição')}}>texto de click</p>
     </div>
   )
-}
+} 
