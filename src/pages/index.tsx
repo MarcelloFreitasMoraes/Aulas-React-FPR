96eac9948161211dba4f79d02728e8554069801b
Marcelo <programadormarcelo1@gmail.com>
2023-02-25 15:29:07 -0300

    map pratica

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,24 +5,42 @@ export default function Home() {
 
   const celulas = [
     {
-      lider: 'Fulano',
-      numero_celula: 10,
-      mais_de_10: true,
-      membros: ['abreu']
+      nome: 'Marcelo',
+      idade: '31 anos',
+      data_de_nascimento: '14/01/1922',
+      nome_mae: 'Maria Elena',
+      nome_pai: 'Marcelo',
     },
     {
-      lider: "Fulano 2",
-      numero_celula: 11,
-      mais_de_10: false,
-      membros: ["joao", "maria", "zezinho"]
+      nome: 'Karina',
+      idade: '26 anos',
+      data_de_nascimento: '17/06/1996',
+      nome_mae: 'Valdirene',
+      nome_pai: 'Sebastião',
     },
     {
-      lider: "Fulano 3",
-      numero_celula: 12,
-      mais_de_10: false,
-      membros: ["jose", "mario", "luiza"]
+      nome: 'Théo',
+      idade: '4 anos',
+      data_de_nascimento: '17/03/2018',
+      nome_mae: 'Marcelo',
+      nome_pai: 'Karina',
+    },
+    {
+      nome: 'Eduarda',
+      idade: '11 anos',
+      data_de_nascimento: '30/06/2011',
+      nome_mae: 'Karina',
+    },
+    {
+      nome: 'Davi Marcelo',
+      idade: '8 anos',
+      data_de_nascimento: '15/06/2014',
+      nome_mae: 'Karina',
     }
   ]
+//exibir uma lista mostrando o nome de 5 pessoas com o nome, idade, data de nascimento e nome da mãe, e para 3 pessoas
+//o nome do Pai porem quem não tiver o pai não tem o titulo pai
+
   return (
     <div className={styles.container}>
       <Head>
@@ -37,9 +55,11 @@ export default function Home() {
           <div 
           className={styles.box}
           key={index}>
-            <p>{item.lider}</p>
-            <p>{item.numero_celula}</p>
-            {item.mais_de_10 && <span>Lider Bom</span>}
+            <p>{item.nome}</p>
+            <p>{item.idade}</p>
+            <p>{item.data_de_nascimento}</p>
+            <p>{item.nome_mae}</p>
+            <p>{item.nome_pai}</p>
           </div>
         )
       })}
