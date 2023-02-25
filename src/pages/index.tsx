743085366a1c8d5267b6ab19f4c62e802cb8b837
Marcelo <programadormarcelo1@gmail.com>
2023-02-25 17:08:53 -0300

    aula 6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,22 +11,23 @@ export default function Home() {
 
 //fazer um click numa div com texto qualquer que retorne um alert o texto "Você conseguiu" 
 // de uma variavel
-  
+const message = "Você conseguiu"
+const number = 4
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Aula-React-FPR | Filter</title>
+        <title>Aula-React-FPR | onChange e onClick</title>
         <meta name="description" content="Aula-React-FPR" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
      <form className={styles.box}>
-      <label>Nome:</label>
-      <input type="text" onChange={(e) => console.log(e.target.value)} />
-      <button onClick={() => {alert('você enviou a requisição')}}>Enviar</button>
+      <label>Digite um número:</label>
+      <input type="text" onChange={(e) => { alert(JSON.parse(e.target.value) + number) }} />
+      <div className={styles.boxDiv} onClick={() => {alert(message)}}>Click Aqui</div>
      </form>
-     <p onClick={() => {alert('você enviou a requisição')}}>texto de click</p>
     </div>
   )
 } 
